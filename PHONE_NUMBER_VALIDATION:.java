57cc94e3d19b0b6d4b6dd5fa7cf5875d77ce53ce
varshini079 <104311746+varshini079@users.noreply.github.com>
2022-09-30 22:50:10 +0530

    import java.util.Scanner; class array {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         long n=sc.nextLong();         if (n>999999999)         {             System.out.println("Valid");         }         else         {         System.out.println("Invalid");         }
        }
    }

---
+++ PHONE_NUMBER_VALIDATION:.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class array
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        long n=sc.nextLong();
+        if (n>999999999)
+        {
+            System.out.println("Valid");
+        }
+        else
+        {
+        System.out.println("Invalid");
+        }
+        
+        
+        
+    }
+}
