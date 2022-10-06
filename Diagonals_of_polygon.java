83d61ef09757d7ff43209c9297a3b1df69bd204e
varshini079 <104311746+varshini079@users.noreply.github.com>
2022-10-06 19:43:21 +0530

    import java.util.*; class fence{     public static void main(String args[]){         Scanner sc=new Scanner (System.in);         int n=sc.nextInt();         int d=(n*(n-3))/2;         if(n>2)         System.out.print(d);
        }
    }

---
+++ Diagonals_of_polygon.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class fence{
+    public static void main(String args[]){
+        Scanner sc=new Scanner (System.in);
+        int n=sc.nextInt();
+        int d=(n*(n-3))/2;
+        if(n>2)
+        System.out.print(d);
+        
+        
+        
+    }
+}
