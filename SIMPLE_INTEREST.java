6682c8f54f231c04a5b191e8c29fb2d1db016b26
varshini079 <104311746+varshini079@users.noreply.github.com>
2022-10-06 19:37:58 +0530

    import java.util.*; class fence{     public static void main(String args[]){         Scanner sc=new Scanner (System.in);         int f=sc.nextInt();         int c=sc.nextInt();         int r=sc.nextInt();         System.out.print((f*c*r)/100);
        }
    }

---
+++ SIMPLE_INTEREST.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class fence{
+    public static void main(String args[]){
+        Scanner sc=new Scanner (System.in);
+        int f=sc.nextInt();
+        int c=sc.nextInt();
+        int r=sc.nextInt();
+        System.out.print((f*c*r)/100);
+        
+        
+        
+    }
+}
