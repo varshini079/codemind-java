4f66511fef4f635779c1a8971913c3440f31b606
varshini079 <104311746+varshini079@users.noreply.github.com>
2022-10-06 19:26:01 +0530

    import java.util.*; class fence{     public static void main(String args[]){         Scanner sc=new Scanner (System.in);         double c=sc.nextDouble();         double f;
            f=((c*(1.8))+32);
            System.out.format("%.2f",f);
            
            
            
        }
    }

---
+++ Celsius_To_Fahrenheit.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class fence{
+    public static void main(String args[]){
+        Scanner sc=new Scanner (System.in);
+        double c=sc.nextDouble();
+        double f;
+        
+        f=((c*(1.8))+32);
+        System.out.format("%.2f",f);
+        
+        
+        
+    }
+}
