7b1a9f36a825cdff1b56375173ef3e3567fab9be
varshini079 <104311746+varshini079@users.noreply.github.com>
2022-10-06 12:34:56 +0530

    import java.util.Scanner; class Array {     public static void main(String args[])     {   int c=0;         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int[] ar=new int[n];         for(int i=0;i<n;i++)         ar[i]=sc.nextInt();         int k=sc.nextInt();         for(int j=0;j<n;j++)         {             if(ar[j]==k)             {System.out.print("True");             break;             }             c=c+1;         }             if(c==n)             System.out.print("False");
        }
    }

---
+++ element_search_in_an_array.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class Array
+{   
+    public static void main(String args[])
+    {   int c=0;
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] ar=new int[n];
+        for(int i=0;i<n;i++)
+        ar[i]=sc.nextInt();
+        int k=sc.nextInt();
+        for(int j=0;j<n;j++)
+        {
+            if(ar[j]==k)
+            {System.out.print("True");
+            break;
+            }
+            c=c+1;
+        }
+            if(c==n)
+            System.out.print("False");
+        
+      
+        
+    }
+}
